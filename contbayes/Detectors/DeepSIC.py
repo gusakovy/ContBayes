5464079ff1d3810ce06c49a245029883ff7c0dd0
gusakovy <116017933+gusakovy@users.noreply.github.com>
2024-02-08 00:25:30 +0200

    Title: Implement frequentist DeepSIC model for comparisons.
    feat: Implement frequentist (non-Bayesian) `DeepSIC` class with offline SGD training function.
    refactor: Move `DeepSICNetBuilder` to a separate file and rename it `DeepSICblock`.

--- contbayes/Detectors/DeepSIC.py
+++ contbayes/Detectors/DeepSIC.py
@@ -1,20 +1,13 @@
-from contbayes.BNN.BayesNN import FullCovBNN
-from contbayes.Detectors.DeepSICblock import DeepSICblock
 from dir_definitions import MODULATION_TYPE
-from functools import partial
+from contbayes.Detectors.DeepSICblock import DeepSICblock
 
 import torch
 from torch import Tensor
 import torch.utils.data as data
-
-import pyro
-import pyro.distributions as dist
-import pyro.infer.autoguide as ag
-
-import tyxe
+from torch.nn.functional import one_hot
 
 
-class BayesianDeepSIC:
+class DeepSIC:
     def __init__(self,
                  num_classes: int,
                  num_users: int,
@@ -28,26 +21,11 @@ class BayesianDeepSIC:
         self.num_layers = num_iterations
         self.rx_size = num_ant if MODULATION_TYPE == "BPSK" else 2 * self.num_ant
         self.verbose = verbose
-        prior = tyxe.priors.IIDPrior(dist.Normal(0, 1))
-        observation_model = tyxe.likelihoods.Categorical(dataset_size=1000, logit_predictions=False)
-        guide = partial(ag.AutoMultivariateNormal, init_scale=1e-4)
-        self.bnn_block = FullCovBNN(model_type="classifier",
-                                    output_dim=4,
-                                    net_builder=partial(DeepSICblock,
-                                                        num_classes=self.num_classes,
-                                                        num_users=self.num_users,
-                                                        num_ant=self.num_ant,
-                                                        hidden_dim=hidden_dim),
-                                    prior=prior,
-                                    likelihood=observation_model,
-                                    guide_builder=guide)
-        # init parameters
-        if self.verbose:
-            print(self.bnn_block.torch_net)
-        zero_input = torch.zeros(1, self.rx_size + (self.num_classes - 1) * (self.num_users-1))
-        self.bnn_block.predict(zero_input)
-        self.param_matrix = [[self.bnn_block.get_params() for _ in range(self.num_layers)]
-                             for _ in range(self.num_users)]
+        self.detector = [[DeepSICblock(num_classes=self.num_classes,
+                                       num_users=self.num_users,
+                                       num_ant=self.num_ant,
+                                       hidden_dim=hidden_dim) for _ in range(self.num_layers)]
+                         for _ in range(self.num_users)]
 
     def pred_and_rx_to_input(self, layer_num, rx, pred=None):
         if layer_num == 0:
@@ -68,14 +46,14 @@ class BayesianDeepSIC:
 
         for user_idx in range(self.num_users):
             user_indices = range((self.num_classes - 1) * user_idx, (self.num_classes - 1) * (user_idx + 1))
-            self.bnn_block.set_params(self.param_matrix[user_idx][layer_num])
             user_inputs = inputs[..., [i for i in range(self.rx_size + (self.num_classes - 1) * self.num_users)
                                        if i not in user_indices]]
             if truncate:
-                layer_outputs[..., user_indices] = self.bnn_block.predict(user_inputs)[..., :(self.num_classes - 1)]
+                layer_outputs[..., user_indices] = \
+                    self.detector[user_idx][layer_num](user_inputs)[..., :(self.num_classes - 1)]
             else:
                 user_indices = range(self.num_classes * user_idx, self.num_classes * (user_idx + 1))
-                layer_outputs[..., user_indices] = self.bnn_block.predict(user_inputs)
+                layer_outputs[..., user_indices] = self.detector[user_idx][layer_num](user_inputs)
 
         return layer_outputs.detach()
 
@@ -89,13 +67,20 @@ class BayesianDeepSIC:
     def _train_block(self, layer_num, user_num, dataloader, num_epochs=250, lr=7e-4, callback=None) -> None:
         if self.verbose:
             print(f"Training block [user = {user_num}][layer = {layer_num}]")
-        optimizer = pyro.optim.Adam({"lr": lr})
-        self.bnn_block.set_params(self.param_matrix[user_num][layer_num])
-        self.bnn_block.fit(data_loader=dataloader,
-                           optim=optimizer,
-                           num_epochs=num_epochs,
-                           callback=None if callback is None else partial(callback, user=user_num, layer=layer_num))
-        self.param_matrix[user_num][layer_num] = self.bnn_block.get_params()
+        optimizer = torch.optim.Adam(self.detector[user_num][layer_num].parameters(), lr=lr)
+        criterion = torch.nn.CrossEntropyLoss()
+        for i in range(num_epochs):
+            running_loss = 0
+            for batch in dataloader:
+                rx, labels = batch
+                optimizer.zero_grad()
+                pred = self.detector[user_num][layer_num].forward(rx)
+                loss = criterion(input=pred, target=labels)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+            if callback is not None:
+                callback(i, self.detector[user_num][layer_num], running_loss, user_num, layer_num)
 
     def _train_layer(self, layer_num, rx, labels, pred=None, num_epochs=250,
                      lr=7e-4, batch_size=None, callback=None) -> Tensor:
@@ -109,7 +94,7 @@ class BayesianDeepSIC:
 
             user_inputs = inputs[..., [i for i in range(self.rx_size + (self.num_classes - 1) * self.num_users)
                                        if i not in user_indices]]
-            user_labels = labels[..., user_idx].squeeze()
+            user_labels = one_hot(labels[..., user_idx].squeeze(), self.num_classes).type(Tensor)
             dataset = data.TensorDataset(user_inputs, user_labels)
             loader = data.DataLoader(dataset, batch_size=batch_size)
             self._train_block(layer_num=layer_num,
@@ -143,7 +128,7 @@ class BayesianDeepSIC:
         return bit_error_rate, confidence
 
     def save_model(self, path):
-        torch.save(self.param_matrix, path)
+        torch.save(self.detector, path)
 
     def load_model(self, path):
-        self.param_matrix = torch.load(path)
+        self.detector = torch.load(path)
